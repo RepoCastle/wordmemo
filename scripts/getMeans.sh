f599b9026358597224c414b96bc862855accb729
Huajian Mao <huajianmao@gmail.com>
2014-01-21 20:05:29 +0800

    I don't know what happened

--- scripts/getMeans.sh
+++ scripts/getMeans.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
 wordfile=$1;
-baseurl="http://dict.youdao.com/search?le=eng&keyfrom=dict.top&q="
+baseurl="http://fanyi.youdao.com/openapi.do?keyfrom=CourseRanking&key=878350469&type=data&doctype=jsonp&callback=?&version=1.1&q="
+
 while read line; do
     for word in $line; do
         url=$baseurl$word
-        echo wget $url -O $word.html;
+        echo "Getting the translation for #" $word "#"
+        wget "$url" -O result/$word.json
+        echo "Sleep 5 seconds ..."
+        sleep 5
     done
 done < $wordfile
